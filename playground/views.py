87e1b2afde28e426c3c2808b7014cab5997b5c35
ankit02327 <ankit2327@protonmail.com>
2025-12-03 15:15:34 +0530

    Create a template
    - Create a template for rendering html code

--- playground/views.py
+++ playground/views.py
@@ -3,8 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def say_hello(request):
-    # The following can be done (a simple explanation, not related to function)
-    # Pull data from db
-    # Transform data 
-    # Send email 
-    return HttpResponse('Hello World!')
+    return render(request, 'hello.html')
