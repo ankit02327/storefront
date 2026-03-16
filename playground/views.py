b82a35de5b2816e571b59e1a35487eb45c676094
ankit02327 <ankit2327@protonmail.com>
2026-03-17 03:48:06 +0530

    Add discounted price annotation for products

--- playground/views.py
+++ playground/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.db.models import Q, F, Value, Func, Count
+from django.db.models import Q, F, Value, Func, Count, ExpressionWrapper, DecimalField
 from django.db.models.functions import Concat 
 from django.db.models.aggregates import Count, Max, Min, Avg, Sum
 from store.models import Product, OrderItem, Customer
-# from django.db.models.aggregates import Count 
+
 def say_hello(request):
-    queryset = Customer.objects.annotate(
-        orders_count=Count('order')
-    )
+    discounted_price = ExpressionWrapper(F('price')*0.8, output_field=DecimalField())
+    queryset = Product.objects.annotate(
+        discounted_price = discounted_price)
     return render(request, 'hello.html', { 'name': 'Ankit', 'result': queryset})
