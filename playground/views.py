34d200194403eda98bb83f7822ddd16bf6a62353
ankit02327 <ankit2327@protonmail.com>
2026-03-17 03:36:03 +0530

    Add Count annotation for customer orders

--- playground/views.py
+++ playground/views.py
@@ -1,16 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.db.models import Q, F, Value, Func
+from django.db.models import Q, F, Value, Func, Count
 from django.db.models.functions import Concat 
 from django.db.models.aggregates import Count, Max, Min, Avg, Sum
 from store.models import Product, OrderItem, Customer
-
+# from django.db.models.aggregates import Count 
 def say_hello(request):
     queryset = Customer.objects.annotate(
-        # CONCAT
-        full_name=Func(F('first_name'),Value(' ') ,
-                       F('last_name'), function='CONCAT'))
-
-    # queryset = Customer.objects.annotate(
-    #   full_name=Concat('first_name',Value(' '),'last_name')
+        orders_count=Count('order')
+    )
     return render(request, 'hello.html', { 'name': 'Ankit', 'result': queryset})
