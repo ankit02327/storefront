2fce92dfd99fdc6119d65d391a4dd80726544a03
ankit02327 <ankit2327@protonmail.com>
2026-03-17 02:20:00 +0530

    Add annotate() example with Value and Customer queryset

--- playground/views.py
+++ playground/views.py
@@ -1,13 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.db.models import Q, F
+from django.db.models import Q, F, Value
 from django.db.models.aggregates import Count, Max, Min, Avg, Sum
-from store.models import Product, OrderItem
+from store.models import Product, OrderItem, Customer
 
 def say_hello(request):
-    result = Product.objects.aggregate(Count('id'))
-
-    # result = Product.objects.filter(collect__id=1).aggregate(count=Count('id'), min_price=Min(price))
-    # result = Product.objects.aggregate(count=Count('id'), min_price=Min(price))
-    # result = Product.objects.aggregate(count=Count('id'))
-    return render(request, 'hello.html', { 'name': 'Ankit', 'result': result})
+    queryset = Customer.objects.annotate(is_new=Value(True))
+    # queryset = Customer.objects.annotate(new_id=F('id')+1)
+    # queryset = Customer.objects.annotate(new_id=F('id'))
+    return render(request, 'hello.html', { 'name': 'Ankit', 'result': queryset})
