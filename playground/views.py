86b0fa1cb2f7f8ba134f11dfe26251052c32b244
ankit02327 <ankit2327@protonmail.com>
2026-03-17 02:10:50 +0530

    Use aggregate() to count products and simplify template

--- playground/views.py
+++ playground/views.py
@@ -1,19 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.db.models import Q, F
+from django.db.models.aggregates import Count, Max, Min, Avg, Sum
 from store.models import Product, OrderItem
 
 def say_hello(request):
-    # select_related (1)
-    # prefetch_related (n)
-    query_set = Product.objects.select_related('collection').all()
+    result = Product.objects.aggregate(Count('id'))
 
-    # query_set = Order.objects.select_related('customer').prefetch_related('orderitem_set__product').order_by('-placed_at')[:5]
-    # query_set = Product.objects.prefetch_related('promotions').select_related('collection').all()
-    # query_set = Product.objects.prefetch_related('promotions').all()
-    # query_set = Product.objects.all()
-
-    for product in query_set:
-        print(product)
-
-    return render(request, 'hello.html', { 'name': 'Ankit', 'products': list(query_set)})
+    # result = Product.objects.filter(collect__id=1).aggregate(count=Count('id'), min_price=Min(price))
+    # result = Product.objects.aggregate(count=Count('id'), min_price=Min(price))
+    # result = Product.objects.aggregate(count=Count('id'))
+    return render(request, 'hello.html', { 'name': 'Ankit', 'result': result})
