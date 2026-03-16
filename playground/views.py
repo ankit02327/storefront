1e1885d3fbfc6f75d8016493944ee7f2c4a272b3
ankit02327 <ankit2327@protonmail.com>
2026-03-17 04:16:42 +0530

    Add example for creating Collection and assigning featured product

--- playground/views.py
+++ playground/views.py
@@ -4,10 +4,19 @@ from django.db.models import Q, F, Value, Func, Count, ExpressionWrapper, Decima
 from django.db.models.functions import Concat 
 from django.db.models.aggregates import Count, Max, Min, Avg, Sum
 from django.contrib.contenttypes.models import ContentType 
-from store.models import Product, OrderItem, Customer
+from store.models import Product, OrderItem, Customer, Order
 from tags.models import TaggedItem
 
 def say_hello(request):
-    content_type = ContentType.objects.get_for_model(Product)
+    collection = Collection()
+    # collection = Collection(title='Video Games')
+    collection.title = 'Video Games'
+    collection.featured_product = Product(pk=1)
+    # collection.featured_product_id = 1
+    collection.save()
+    collection.id
+
+    # collection = Collection.objects.create(name='a', featured_product_id=1)
+    # collection.id 
     
     return render(request, 'hello.html', { 'name': 'Ankit', 'tags': queryset})
