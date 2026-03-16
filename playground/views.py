61c0346f253b1b963e66624440aade96af8c4d6c
ankit02327 <ankit2327@protonmail.com>
2026-03-17 04:29:21 +0530

    Show updating an existing Collection and clearing featured product

--- playground/views.py
+++ playground/views.py
@@ -4,19 +4,21 @@ from django.db.models import Q, F, Value, Func, Count, ExpressionWrapper, Decima
 from django.db.models.functions import Concat 
 from django.db.models.aggregates import Count, Max, Min, Avg, Sum
 from django.contrib.contenttypes.models import ContentType 
-from store.models import Product, OrderItem, Customer, Order
+from store.models import Product, OrderItem, Customer, Order, Collection
 from tags.models import TaggedItem
 
 def say_hello(request):
-    collection = Collection()
+    collection = Collection(pk=11)
     # collection = Collection(title='Video Games')
+    # collection = Collection.objects.get(pk=11)
     collection.title = 'Video Games'
-    collection.featured_product = Product(pk=1)
+    collection.featured_product = None
     # collection.featured_product_id = 1
     collection.save()
     collection.id
-
+    
+    # Collection.objects.filter(pk=11).update(featured_product=None)
     # collection = Collection.objects.create(name='a', featured_product_id=1)
     # collection.id 
     
-    return render(request, 'hello.html', { 'name': 'Ankit', 'tags': queryset})
+    return render(request, 'hello.html', { 'name': 'Ankit'})
