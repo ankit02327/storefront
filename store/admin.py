036ce87c1ad14ad97f72134aefb1e8099fffea78
ankit02327 <ankit2327@protonmail.com>
2026-03-17 11:18:12 +0530

    Improve admin with autocomplete, slug prefill and search

--- store/admin.py
+++ store/admin.py
@@ -19,6 +19,11 @@ class InventoryFilter(admin.SimpleListFilter):
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
+    autocomplete_fields = ["collection"]
+    prepopulated_fields = {"slug": ["title"]}
+    # fields = ["title", "slug"]
+    exclude = ["promotions"]
+    # readonly_fields = ['title']
     actions = ["clear_inventory"]
     list_display = ["title", "price", "inventory_status", "collection_title"]
     list_editable = ["price"]
@@ -62,6 +67,7 @@ class OrderAdmin(admin.ModelAdmin):
 @admin.register(models.Collection)
 class CollectionAdmin(admin.ModelAdmin):
     list_display = ["title", "products_count"]
+    search_fields = ["title"]
 
     @admin.display(ordering="products_count")
     def products_count(self, collection):
