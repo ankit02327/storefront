4c23a70935335a4a45a5e810e664bf5946abb9f9
ankit02327 <ankit2327@protonmail.com>
2026-03-18 02:30:44 +0530

    Convert views to DRF and add product detail endpoint

--- store/urls.py
+++ store/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 from . import views
 
 # URLConf module
-urlpatterns = [path("products/", views.product_list)]
+urlpatterns = [
+    path("products/", views.product_list),
+    path("products/<int:id>/", views.product_detail),
+]
