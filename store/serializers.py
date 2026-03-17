b1c560ea8d60db61a9abc27a36522dd5fb9b0396
ankit02327 <ankit2327@protonmail.com>
2026-03-18 04:39:23 +0530

    Add collection relation to ProductSerializer and optimize queries

--- store/serializers.py
+++ store/serializers.py
@@ -1,6 +1,11 @@
 from decimal import Decimal
-from store.models import Product
+from store.models import Product, Collection
 from rest_framework import serializers
+
+
+class CollectionSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    title = serializers.CharField(max_length=255)
 
 
 class ProductSerializer(serializers.Serializer):
@@ -9,6 +14,11 @@ class ProductSerializer(serializers.Serializer):
     price = serializers.DecimalField(max_digits=6, decimal_places=2)
     # unit_price = serializers.DecimalField(max_digits=6, decimal_places=2,source='price')
     price_with_tax = serializers.SerializerMethodField(method_name="calculate_tax")
+    collection = serializers.HyperlinkedRelatedField(
+        queryset=Collection.objects.all(), view_name="collection-detail"
+    )
+    # collection = CollectionSerializer()
+    # collection = serializers.PrimaryKeyRelatedField(queryset=Collection.objects.all())
 
     def calculate_tax(self, product: Product):
         return product.price * Decimal(1.1)
