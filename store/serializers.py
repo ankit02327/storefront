90f172877d8cee990943b3756da46d74c958c09a
ankit02327 <ankit2327@protonmail.com>
2026-03-18 04:54:20 +0530

    Convert serializers to ModelSerializer

--- store/serializers.py
+++ store/serializers.py
@@ -3,22 +3,20 @@ from store.models import Product, Collection
 from rest_framework import serializers
 
 
-class CollectionSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Collection
+        fields = ["id", "title"]
 
 
-class ProductSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
-    price = serializers.DecimalField(max_digits=6, decimal_places=2)
-    # unit_price = serializers.DecimalField(max_digits=6, decimal_places=2,source='price')
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ["id", "title", "price", "price_with_tax", "collection"]
+
+    # # unit_price = serializers.DecimalField(max_digits=6, decimal_places=2,source='price')
     price_with_tax = serializers.SerializerMethodField(method_name="calculate_tax")
-    collection = serializers.HyperlinkedRelatedField(
-        queryset=Collection.objects.all(), view_name="collection-detail"
-    )
-    # collection = CollectionSerializer()
-    # collection = serializers.PrimaryKeyRelatedField(queryset=Collection.objects.all())
 
+    # # collection = serializers.PrimaryKeyRelatedField(queryset=Collection.objects.all())
     def calculate_tax(self, product: Product):
         return product.price * Decimal(1.1)
