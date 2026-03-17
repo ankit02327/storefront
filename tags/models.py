688a895b30ee27a9fff2d71777ccb6fc0cc3437b
ankit02327 <ankit2327@protonmail.com>
2026-03-17 13:53:45 +0530

    Extend tags app with admin and generic tagging support

--- tags/models.py
+++ tags/models.py
@@ -3,29 +3,32 @@ from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 
+
 class LikedItem(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey()
-    
+
+
 class TaggedItemManager(models.Manager):
     def get_tags_for(self, obj_type, obj_id):
         content_type = ContentType.objects.get_for_model(obj_type)
 
-        return TaggedItem.objects \
-            .select_related('tag') \
-            .filter(
-            content_type=content_type,
-            object_id=obj_id
-            )
+        return TaggedItem.objects.select_related("tag").filter(
+            content_type=content_type, object_id=obj_id
+        )
 
 
 class Tag(models.Model):
     label = models.CharField(max_length=255)
 
+    def __str__(self) -> str:
+        return self.label
+
+
 class TaggedItem(models.Model):
-    objects = TaggedItemManager() 
+    objects = TaggedItemManager()
     # Which tag is applied to which object
     tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
     # Type (product, video, article)
