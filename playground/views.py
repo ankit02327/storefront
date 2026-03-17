f8abbaa722066a1ee780ab1e28717ce36b8af420
ankit02327 <ankit2327@protonmail.com>
2026-03-17 06:05:07 +0530

    Initial Config for Admin

--- playground/views.py
+++ playground/views.py
@@ -10,8 +10,8 @@ from tags.models import TaggedItem
 
 def say_hello(request):
     queryset = Product.objects.raw('SELECT * FROM store_product')
-    with connection.cursor() as cursor:
-        cursor.execute()
+    # with connection.cursor() as cursor:
+    #    cursor.execute()
 
         # cursor.callproc('get_customers', [1,2])
     return render(request, 'hello.html', { 'name': 'Ankit', 'result': list(queryset)})
