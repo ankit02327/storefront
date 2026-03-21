f6c2e04ed28d694d0345bd2d275c8c1f1450f470
ankit02327 <ankit2327@protonmail.com>
2026-03-21 12:00:53 +0530

    add create order logic

--- store/views.py
+++ store/views.py
@@ -19,6 +19,7 @@ from .serializers import (
     UpdateCartItemSerializer,
     CustomerSerializer,
     OrderSerializer,
+    CreateOrderSerializer,
 )
 from .filters import ProductFilter
 from .pagination import DefaultPagination
@@ -140,8 +141,15 @@ class CustomerViewSet(ModelViewSet):
 
 
 class OrderViewSet(ModelViewSet):
-    serializer_class = OrderSerializer
     permission_classes = [IsAuthenticated]
+
+    def get_serializer_class(self):
+        if self.request.method == "POST":
+            return CreateOrderSerializer
+        return OrderSerializer
+
+    def get_serializer_context(self):
+        return {"user_id": self.request.user.id}
 
     def get_queryset(self):
         user = self.request.user
