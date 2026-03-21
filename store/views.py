63fdd3f63b235cd71a9fa5fb29a76a15b4e66ee9
ankit02327 <ankit2327@protonmail.com>
2026-03-21 07:17:36 +0530

    add current user profile endpoint

--- store/views.py
+++ store/views.py
@@ -12,6 +12,7 @@ from .serializers import (
 )
 from .filters import ProductFilter
 from .pagination import DefaultPagination
+from rest_framework.decorators import action
 from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.mixins import (
     CreateModelMixin,
@@ -104,3 +105,15 @@ class CustomerViewSet(
 ):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
+
+    @action(detail=False, methods=["GET", "PUT"])
+    def me(self, request):
+        (customer, created) = Customer.objects.get_or_create(user_id=request.user.id)
+        if request.method == "GET":
+            serializer = CustomerSerializer(customer)
+            return Response(serializer.data)
+        elif request.method == "PUT":
+            serializer = CustomerSerializer(customer, data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data)
